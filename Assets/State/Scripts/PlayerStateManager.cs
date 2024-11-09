65519a26cc50b4764e17a5743b8341257e048571
1609Dzuaa <datrua3152003@gmail.com>
2024-11-09 11:49:16 +0700

    up normal approach using state machine

--- Assets/State/Scripts/PlayerStateManager.cs
+++ Assets/State/Scripts/PlayerStateManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -23,7 +23,25 @@ public class PlayerStateManager : MonoBehaviour
     public float MoveSpeed, RunSpeed, JumpForce, GCheckDist;
     public LayerMask GroundLayer;
     #endregion
+
+    #region State Pattern
     BaseState _state;
+    #endregion
+
+    #region State Machine
+    int state;
+    #endregion
+
+    #region State Constant
+
+    const int IDLE_STATE = 0;
+    const int WALK_STATE = 1;
+    const int RUN_STATE = 2;
+    const int JUMP_STATE = 3;
+    const int FALL_STATE = 4;
+    const int SIT_STATE = 5;
+
+    #endregion
 
     void Start()
     {
@@ -55,12 +73,15 @@ public class PlayerStateManager : MonoBehaviour
     {
         ReadInput();
         Flip();
-        _state?.Update();
+        //_state?.Update();
+        UpdateLogic();
+        UpdateAnimation();
     }
 
     private void FixedUpdate()
     {
         CheckGround();
+        //UpdatePhysics(); //un-comment khi trình bày cách tiếp cận bthg
     }
 
     private void ReadInput()
@@ -74,4 +95,62 @@ public class PlayerStateManager : MonoBehaviour
         isOnGround = Physics2D.Raycast(GroundCheck.position, Vector2.down, GCheckDist, GroundLayer);
         //Debug.Log("isG: " + isOnGround);
     }
+
+    #region No State Pattern
+
+    private void UpdateAnimation()
+    {
+        if (state == SIT_STATE)
+            Anim.SetInteger("state", (int)EState.PlayerState.Sit);
+        else if (state == JUMP_STATE)
+            Anim.SetInteger("state", (int)EState.PlayerState.Jump);
+        else if (state == WALK_STATE)
+            Anim.SetInteger("state", (int)EState.PlayerState.Walk);
+        else if (state == IDLE_STATE)
+            Anim.SetInteger("state", (int)EState.PlayerState.Idle);
+        else if (state == FALL_STATE)
+            Anim.SetInteger("state", (int)EState.PlayerState.Fall);
+    }
+
+    private void UpdatePhysics()
+    {
+        if (state != SIT_STATE && state != IDLE_STATE)
+        {
+           if (state == JUMP_STATE)
+                Rb2D.velocity = new Vector2(DirX * MoveSpeed, JumpForce);
+            else
+                Rb2D.velocity = new Vector2(DirX * MoveSpeed, Rb2D.velocity.y);
+        }
+        else
+        {
+            Rb2D.velocity = Vector2.zero;
+        }
+    }
+
+    private void UpdateLogic()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) && isOnGround)
+            SwitchState(SIT_STATE);
+        else if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+            SwitchState(JUMP_STATE);
+        else if (DirX != 0 && isOnGround)
+            SwitchState(WALK_STATE);
+        else if (!isOnGround && Rb2D.velocity.y < -0.1f)
+            SwitchState(FALL_STATE);
+        else if (isOnGround && Mathf.Abs(DirX) == 0 && Rb2D.velocity.y <= 0f)
+            SwitchState(IDLE_STATE);
+        //Debug.Log("state: " + state);
+    }
+
+    private void SwitchState(int State)
+    {
+        state = State;
+        if (state == SIT_STATE)
+            Collider2D.size = new Vector2(0.875f, 1.15f);
+        else
+            Collider2D.size = new Vector2(0.875f, 1.6875f);
+        UpdatePhysics();
+    }
+
+    #endregion
 }
